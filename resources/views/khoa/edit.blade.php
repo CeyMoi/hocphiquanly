f63f42f1e0e3d54b62e39f6dd0a2344f76ed9705
Phạm Quang Hữu <phamhuu.a2.zip@gmail.com>
2020-06-29 16:46:53 +0700

    view sửa khóa

---
+++ resources/views/khoa/edit.blade.php
@@ -0,0 +1,14 @@
+<form action="{{ route('khoa.update',['id'=> $khoa->ma_khoa)] }}" method="post">
+    <input type="hidden" name="_method" value="PUT">
+    {{csrf_field()}}
+    Mã khóa <input type="text" name="ma_khoa">
+    <br>
+    Tên khóa <input type="text" name="ten_khoa">
+    <br>
+    Thời gian bắt đầu <input type="text" name="thoi_gian_bat_dau">
+    <br>
+    Thời gian kết thúc <input type="text" name="thoi_gian_ket_thuc">
+
+
+    <button>Sửa</button>
+</form>
