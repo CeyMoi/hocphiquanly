b994317234cb654f25ad4d48b78895803a717f87
Phạm Quang Hữu <phamhuu.a2.zip@gmail.com>
2020-07-01 14:42:29 +0700

    sửa lại 1 chút

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ Route::resource('khoa', 'KhoaController'); /// sửa lại lần nữa
 
 Route::resource('chuong_trinh_dao_tao', 'ChuongTrinhDaoTaoController');
 
-Route::resource('lop', 'Lop');
+Route::resource('lop', 'LopController');
