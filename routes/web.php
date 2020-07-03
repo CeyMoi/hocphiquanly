343ac8bd49a366118d7311089092dd68796d4dfd
Phạm Quang Hữu <phamhuu.a2.zip@gmail.com>
2020-07-03 16:12:23 +0700

    thêm route sinh viên

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ Route::resource('khoa', 'KhoaController'); /// sửa lại lần nữa
 Route::resource('chuong_trinh_dao_tao', 'ChuongTrinhDaoTaoController');
 
 Route::resource('lop', 'LopController');
+
+Route::resource('sinh_vien', 'SinhVienController');
