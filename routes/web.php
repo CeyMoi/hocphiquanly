95ece771fd83851c44d5013e4f315e833e86b4b3
Phạm Quang Hữu <phamhuu.a2.zip@gmail.com>
2020-07-08 13:57:06 +0700

    thêm route giao diện + học phí + hóa đơn

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::resource('chuong_trinh_dao_tao', 'ChuongTrinhDaoTaoController');
 Route::resource('lop', 'LopController');
 
 Route::resource('sinh_vien', 'SinhVienController');
+
+Route::any('hoc_phi', 'HocPhiController@getHocPhi');
+
+Route::resource('hoa_don', 'HoaDonController');
+
+Route::get('test', 'TestController@test');
